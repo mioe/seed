5ee24eabdbe493d51341c595b2e5b52e443e67e2
mioe <mioe.boss@gmail.com>
2022-06-17 10:57:52 +0300

    remove useless docker files & change to mono arch

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,22 +1,14 @@
 import Fastify from 'fastify'
-import { join } from 'path'
-import autoLoad from '@fastify/autoload'
+// import { join } from 'path'
+// import autoLoad from '@fastify/autoload'
 
 const fastify = Fastify({
   logger: true
 })
 
 // Will load all routes under src/routes
-fastify.register(autoLoad, {
-  dir: join(__dirname, 'routes')
-})
+// fastify.register(autoLoad, {
+//   dir: join(__dirname, 'routes')
+// })
 
-const start = async () => {
-  try {
-    await fastify.listen(3000)
-  } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
-}
-start()
+fastify.listen({ port: 3000 })
