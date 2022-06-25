6223863e27e1da42f3fe5019e534a374cbabec20
mioe <mioe.boss@gmail.com>
2022-06-25 17:19:06 +0300

    fix error for load pem

--- backend/src/plugins/jwt.ts
+++ backend/src/plugins/jwt.ts
@@ -1,16 +1,19 @@
 import fp from 'fastify-plugin'
 import { readFileSync } from 'fs'
+import { join } from 'path'
 import type { FastifyPluginCallback } from 'fastify'
+
+const PATH = join(__dirname, '../../config')
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const jwtPlugin: FastifyPluginCallback = fp(async(fastify, opts, done) => {
 	fastify.register(import('@fastify/jwt'), {
 		secret: {
 			private: {
-				key:  readFileSync('../config/private.pem', 'utf8'),
+				key:  readFileSync(`${PATH}/private.pem`, 'utf8'),
 				passphrase: process.env.SECRET_KEY,
 			},
-			public: readFileSync('../config/public.pem', 'utf8'),
+			public: readFileSync(`${PATH}/public.pem`, 'utf8'),
 		},
 		sign: { algorithm: 'RS256' },
 	})
