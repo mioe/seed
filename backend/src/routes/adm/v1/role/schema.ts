c21b15274c749f8d85f97daa55d8fe8424fa9a01
mioe <mioe.boss@gmail.com>
2022-07-07 06:30:11 +0300

    fix $id error

--- backend/src/routes/adm/v1/role/schema.ts
+++ backend/src/routes/adm/v1/role/schema.ts
@@ -2,18 +2,24 @@ import { FromSchema } from 'json-schema-to-ts'
 import type { FastifySchema } from 'fastify'
 
 export const roleCreateInputBody = {
-	$id: 'post',
 	type: 'object',
 	properties: {
 		name: { type: 'string' },
 		description: { type: 'string' },
 		permissions: { type: 'array', items: { type: 'string' } },
 	},
-	example: {
-		name: 'New random role',
-		description: 'Description role',
-		permissions: ['role::read', 'role::create'],
-	},
+	examples: [
+		{
+			name: 'New random role',
+			description: 'Description role',
+			permissions: ['role::read', 'role::create'],
+		},
+		{
+			name: 'New random role2',
+			description: 'Description role2',
+			permissions: ['role::read', 'role::create'],
+		},
+	],
 	required: ['name'],
 } as const
 
@@ -23,9 +29,4 @@ export const roleCreateSchema: FastifySchema = {
 	tags: ['Role'],
 	description: 'Create new role',
 	body: roleCreateInputBody,
-	response: {
-		200: {
-			token: { type: 'string' },
-		},
-	},
 }
