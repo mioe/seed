6d0515e011d8e949c5799d1518bb8c076b13edd8
mioe <mioe.boss@gmail.com>
2022-07-12 18:12:00 +0300

    try this

--- backend/src/routes/root.ts
+++ backend/src/routes/root.ts
@@ -1,8 +1,12 @@
 import type { FastifyPluginAsync } from 'fastify'
+// import { RolePermissions } from '@shared/constants'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const root: FastifyPluginAsync = async(fastify, opts): Promise<void> => {
+
+
 	fastify.get('/', async(request, reply) => {
+		// fastify.log.info('🦕 msg', RolePermissions.View)
 		fastify.log.info('🦕 hello world')
 		reply.send({ root: true })
 	})
