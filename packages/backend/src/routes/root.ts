07af20789111ea828b9092981b42d06056705a3f
mioe <mioe.boss@gmail.com>
2022-07-13 09:25:16 +0300

    remove types folder

--- packages/backend/src/routes/root.ts
+++ packages/backend/src/routes/root.ts
@@ -3,8 +3,6 @@ import { RolePermissions } from '@seed/shared/constants/permissions'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const root: FastifyPluginAsync = async(fastify, opts): Promise<void> => {
-
-
 	fastify.get('/', async(request, reply) => {
 		fastify.log.info('🦕 msg', RolePermissions.View)
 		fastify.log.info('🦕 hello world')
